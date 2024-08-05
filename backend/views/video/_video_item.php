9f897b224e26f4459d9335185a42fa1a71a70c43
David Weiss <david.black.weiss@seznam.cz>
2024-08-05 18:04:02 +0200

    3:22:46, Access osetreni aby se clovek nedostal do videii kdyz neni lognutej

--- backend/views/video/_video_item.php
+++ backend/views/video/_video_item.php
@@ -5,10 +5,12 @@ use \yii\helpers\StringHelper;
 ?>
 
 <div class="media">
-    <div class="embed-responsive embed-responsive-16by9 mr-3"
-         style="width: 140px;">
-        <video poster="<?php echo $model->getThumbnail() ?>" class="embed-responsive-item" src="<?php echo $model->getVideoLink() ?>"></video>
-    </div>
+    <a href="<?php echo \yii\helpers\Url::to(['video/update','video_id'=>$model->video_id]) ?>">
+        <div class="embed-responsive embed-responsive-16by9 mr-3"
+             style="width: 140px;">
+            <video poster="<?php echo $model->getThumbnail() ?>" class="embed-responsive-item" src="<?php echo $model->getVideoLink() ?>"></video>
+        </div>
+    </a>
     <div class="media-body">
         <h6 class="mt-0"><?php echo $model->tittle ?></h6>
         <?php echo StringHelper::truncateWords($model->description,10) ?>
